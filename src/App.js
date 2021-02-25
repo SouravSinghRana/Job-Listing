71ce10c6bf5551959a6e3c42bdd6d5e797886597
SouravSinghRana <41117593+SouravSinghRana@users.noreply.github.com>
2021-02-25 17:21:14 +0530

    added search form for jobs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import useFetchJobs from './useFetchJobs';
 import { Container } from 'semantic-ui-react'
 import Job from './Job';
 import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 const  App = () => {
 
@@ -12,10 +13,20 @@ const  App = () => {
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params , page);
   // console.log(jobs);
 
+  const handleParamChange = (e) => {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
+
   return ( 
       <div className="my-4 mb-10">
           <Container>
             <h1 className="mb-4">Jobs Listing</h1>
+            <SearchForm params={params} onParamChange={handleParamChange} />
             <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
             {loading && <h1>Loading...</h1>}
             {error && <h1>Error. Try Refreshing.</h1>}
