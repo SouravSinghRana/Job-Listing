229a9ecd6ac702fb5b3dfd313fc6a2b79ea8e2cf
SouravSinghRana <41117593+SouravSinghRana@users.noreply.github.com>
2021-04-11 18:17:06 +0530

    proxy updated

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -13,7 +13,7 @@ const ACTIONS = {
   UPDATE_HAS_NEXT_PAGE: 'update-has-next-page'
 }
 
-const BASE_URL = "https://jobs.github.com/positions.json" ; 
+const BASE_URL = "https://thingproxy.freeboard.io/fetch/https://jobs.github.com/positions.json" ; 
 // "https://remotive.io/api/remote-jobs"
 //https://thingproxy.freeboard.io/fetch/
 
